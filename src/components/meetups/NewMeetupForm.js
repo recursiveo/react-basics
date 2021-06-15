8a092c687032060a17ef8d89a7875809aa0681f6
Jomy Jo <jomi.jacob.001@gmail.com>
2021-06-15 13:41:36 +0530

    day4

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,24 +1,46 @@
 import Cards from '../ui/Cards';
 import classes from './NewMeetupForm.module.css';
+import { useRef } from 'react';
 
-function NewMeetupForm(){
+function NewMeetupForm(props){
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descInputRef = useRef();
+
+    function submitHandler(event) {
+        event.preventDefault();
+
+        const titletext = titleInputRef.current.value;
+        const imageUrl = imageInputRef.current.value;
+        const addresstext = addressInputRef.current.value;
+        const descriptiontext = descInputRef.current.value;
+
+        const newMeetup = {
+            title: titletext,
+            image: imageUrl,
+            address: addresstext,
+            description: descriptiontext,
+        }
+        props.addNewMeetup(newMeetup);
+    }
     return <Cards>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
                 <label htmlFor='title'>Title</label>
-                <input type='text' id='title' required/>
+                <input type='text' id='title' required ref={titleInputRef}/>
             </div>
             <div className={classes.control}>
                 <label htmlFor='image'>Image</label>
-                <input type='url' id='image' required/>
+                <input type='url' id='image' required ref={imageInputRef}/>
             </div>
             <div className={classes.control}>
                 <label htmlFor='address'>Address</label>
-                <input type='text' id='address' required/>
+                <input type='text' id='address' required ref={addressInputRef}/>
             </div>
             <div className={classes.control}>
                 <label htmlFor='desc'>Description</label>
-                <textarea id='desc' rows='5' required></textarea>
+                <textarea id='desc' rows='5' required ref={descInputRef}></textarea>
             </div>
             <div className={classes.actions}>
                 <button>Add New Meetup</button>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,24 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage(){
+    function addMeetupHandler(newMeetup) {
+		const SERVER_URL = '' //GET FROM FIREBASE;
+        // let response = fetch(
+        //   SERVER_URL,
+        //     {
+        //         method: 'POST',
+        //         body: JSON.stringify(newMeetup),
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },
+        //     }
+        // );
+        // console.log(response)
+    }
     return (
         <section>
             <h1>NewMeetupPage</h1>
-            <NewMeetupForm/>
+            <NewMeetupForm addNewMeetup={addMeetupHandler}/>
         </section>
     );
 }
