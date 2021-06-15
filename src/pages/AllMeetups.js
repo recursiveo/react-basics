053d83e1a9202059d968464a7308266bb445b48b
Jomy Jo <jomi.jacob.001@gmail.com>
2021-06-15 23:20:07 +0530

    added api call to fetch data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,33 +1,68 @@
 import MeetupList from "../components/meetups/MeetupList";
+import * as data from '../secrets.json';
+import {useState, useEffect} from 'react';
 
-const DUMMY_DATA = [
-    {
-        id: 'm1',
-        title: 'This is a first meetup',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-        id: 'm2',
-        title: 'This is a second meetup',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-];
+// const DUMMY_DATA = [
+//     {
+//         id: 'm1',
+//         title: 'This is a first meetup',
+//         image:
+//             'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//         address: 'Meetupstreet 5, 12345 Meetup City',
+//         description:
+//             'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//     },
+//     {
+//         id: 'm2',
+//         title: 'This is a second meetup',
+//         image:
+//             'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//         address: 'Meetupstreet 5, 12345 Meetup City',
+//         description:
+//             'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//     },
+// ];
 
-function AllMeetupsPage(){
+function AllMeetupsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([])
+
+    useEffect(() => {
+        fetch(data.db_url)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                const allmeetups = [];
+
+                for (const key in data) {
+                    const meetup = {
+                        id: key,
+                        ...data[key],
+                    }
+                    allmeetups.push(meetup);
+                }
+                setLoadedMeetups(allmeetups)
+                setIsLoading(false);
+            });
+    },[]);
+
+    if(isLoading){
+        return <section>
+            <div>
+                <p>
+                    Loading...
+                </p>
+            </div>
+        </section>
+    }
+
     return (
         <section>
             <h1>
                 AllMeetupsPage
             </h1>
-            <MeetupList items={DUMMY_DATA}/>
+            <MeetupList items={loadedMeetups}/>
         </section>
     );
 }
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,19 +1,24 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import * as data from '../secrets.json';
+import { useHistory } from 'react-router-dom';
 
 function NewMeetupPage(){
-    function addMeetupHandler(newMeetup) {
-		const SERVER_URL = '' //GET FROM FIREBASE;
-        // let response = fetch(
-        //   SERVER_URL,
-        //     {
-        //         method: 'POST',
-        //         body: JSON.stringify(newMeetup),
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //         },
-        //     }
-        // );
-        // console.log(response)
+    const history = useHistory();
+
+    async function addMeetupHandler(newMeetup) {
+		const SERVER_URL = data.db_url;     //GET FROM FIREBASE;
+        fetch(
+            SERVER_URL,
+            {
+                method: 'POST',
+                body: JSON.stringify(newMeetup),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        ).then(() => {
+            history.replace('/');
+        });
     }
     return (
         <section>
